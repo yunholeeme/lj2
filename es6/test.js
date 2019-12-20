122bbb38730816503aba3d0b8cd6fe6a46d68955
hodu <neuezeal@gmail.com>
2019-12-20 21:03:08 +0900

    ignored contents of dist and public/dist directory.

---
+++ es6/test.js
@@ -0,0 +1,15 @@
+'use strict';
+// es6 feature: block-scoped "let" declaration
+const sentences = [
+{ subject: 'Leehyerim', verb: 'is', object: 'pretty' },
+{ subject: 'Parkyeji', verb: 'are', object: 'cute' },
+];
+// es6 feature: object destructuring
+function say({ subject, verb, object }) {
+// es6 feature: template strings
+console.log(`${subject} ${verb} ${object}`);
+}
+// es6 feature: for..of
+for(let s of sentences) {
+say(s);
+}
