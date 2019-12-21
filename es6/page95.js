fe022fcd574e88fd232cea230d6d4a1b5073eeca
hodu <neuezeal@gmail.com>
2019-12-21 18:51:27 +0900

    modify es6/page95.js public/es6/page95.js

--- es6/page95.js
+++ es6/page95.js
@@ -3,3 +3,35 @@ console.log(s.toUpperCase());
 
 s.rating = 3;
 console.log(s.rating);
+
+const a2 = [1, "two", 3, null]; // different types
+const a3 = [
+    "I love leehyerim",
+    "I love beakyuri",
+    "I love chominji",
+    "I love janghajin",
+];
+
+const a4 = [
+    {name:"Ruby", hardness: 9},
+    {name:"Diamond", hardness: 10},
+    {name:"Topaz", hardness: 8},
+];
+
+const a5 = [
+    ["💛", "💛", "💛"],
+    ["💛", "💛", "💛"],
+    ["💛", "💛", "💛"],
+];
+
+console.log(a2[1]);
+console.log(a3[1]);
+const sungshin07 = (x) => {
+    for (let s of x) {
+        console.log(s);
+    }
+};
+
+console.log(sungshin07());
+console.log(a5.length);
+
