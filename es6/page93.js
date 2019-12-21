ec4c8c5a6aa4c71b81031148b93fc652650c211a
hodu <neuezeal@gmail.com>
2019-12-21 18:17:02 +0900

    create es6/page93.js

---
+++ es6/page93.js
@@ -0,0 +1,26 @@
+const sam1 = {
+    name: 'Sam',
+    age: 4,
+};
+
+const sam2 = { name: 'Sam', age:4};
+
+const sam3 = {
+    name: 'Sam',
+    classification: {
+        kingdom: 'Anamalia',
+        phylum: 'Chordata',
+        class: 'Mamalia',
+        order: 'Carnivoria',
+        family: 'Felidae',
+        subfamily: 'Felinae',
+        genus: 'Felis',
+        species: 'catus',
+    },
+};
+
+sam1.age;
+sam2.name;
+console.log(sam3.classification.family);
+console.log(sam3.classification['family']);
+console.log(sam3['classification'].family);
