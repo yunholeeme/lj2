642e86a216d0c237bb6c68f8ea2ae8a1ab26d6b3
hodu <neuezeal@gmail.com>
2019-12-21 18:00:22 +0900

    modify .eslintrc.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,23 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const eslint = require('gulp-eslint');
 // Gulp dependencies go here.
 
 gulp.task('default', function(callback) {
     // Gulp tasks go here
 
+    // Run ESLint
+    gulp.src(["es6/**/*.js", "public/es6/**/*.js"])
+        .pipe(eslint())
+        .pipe(eslint.format());
     // node source
     gulp.src("es6/**/*.js")
         .pipe(babel())
         .pipe(gulp.dest("dist"));
-
     // browser source
     gulp.src("public/es6/**/*.js")
         .pipe(babel())
         .pipe(gulp.dest("public/dist"));
-        
+
     callback();
 });
--- public/es6/test.js
+++ public/es6/test.js
@@ -1,15 +1,15 @@
 'use strict';
 // es6 feature: block-scoped "let" declaration
 const sentences = [
-{ subject: 'Leehyerim', verb: 'is', object: 'pretty' },
-{ subject: 'Parkyeji', verb: 'are', object: 'cute' },
+    { subject: 'Leehyerim', verb: 'is', object: 'pretty' },
+    { subject: 'Parkyeji', verb: 'are', object: 'cute' },
 ];
 // es6 feature: object destructuring
 function say({ subject, verb, object }) {
 // es6 feature: template strings
-console.log(`${subject} ${verb} ${object}`);
+    console.log(`${subject} ${verb} ${object}`);
 }
 // es6 feature: for..of
 for(let s of sentences) {
-say(s);
+    say(s);
 }
