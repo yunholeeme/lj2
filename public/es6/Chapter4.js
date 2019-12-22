91dd2a6adeed0957690164cd20cc998ff19d107a
hodu <neuezeal@gmail.com>
2019-12-22 14:56:19 +0900

    modify es6/Chapter4.js public/es6/Chapter4.js

--- public/es6/Chapter4.js
+++ public/es6/Chapter4.js
@@ -9,16 +9,28 @@ let funds = 50;     // start condition
 //     console.log(funds);
 // }
 
-console.log("start");
-while (     funds > 1 &&            funds < 100) {
-    console.log(funds);
+// console.log("start");
+// while (     funds > 1 &&            funds < 100) {
+//     console.log(funds);
 
 
 
 
-    funds = funds + 2;
+//     funds = funds + 2;
+// }
+
+
+
+// console.log(funds);
+
+// return >= m && <= n random int
+function rand(m, n) {
+    return m + Math.floor((n - m + 1)*Math.random());
 }
 
+// 크라운 앤 앵커 게임의 여섯 가지 도형 중 하나를 무작위로 반환합니다.
+function randFace() {
+    return ["crown", "anchor", "heart", "spade", "club", "diamond"][rand(0, 5)];
+}
 
-
-console.log(funds);
+randFace();
