016cb2bbff33fb2a96fe1c50e91a7ea5631b7b0c
hodu <neuezeal@gmail.com>
2019-12-22 15:31:03 +0900

    modify es6/Chapter4.js public/es6/Chapter4.js

--- es6/Chapter4.js
+++ es6/Chapter4.js
@@ -37,6 +37,7 @@ function randFace() {
 
 const bets = { crown: 0, anchor: 0, heart: 0, spade: 0, club: 0, diamond: 0};
 let totalBet = rand(1, funds);
+console.log(`start : ${totalBet}`);
 if (totalBet === 7) {
     totalBet = funds;
     bets.heart = totalBet;
