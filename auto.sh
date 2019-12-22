216961a074feeb59d8858339c94ccb507bf37cc0
hodu <neuezeal@gmail.com>
2019-12-22 13:54:30 +0900

    modify auto.sh

--- auto.sh
+++ auto.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cp es6/page95.js public/es6/page95.js
+cp es6/Chapter4.js public/es6/Chapter4.js
 git status
 git add -A
-git commit -m "modify es6/page95.js public/es6/page95.js"
+git commit -m "modify es6/Chapter4.js public/es6/Chapter4.js"
 git status
 node es6/page95.js
 node public/es6/page95.js
