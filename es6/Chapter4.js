89abc1631086669eb5fb8612ce5e88bcacefea00
hodu <neuezeal@gmail.com>
2019-12-22 15:00:22 +0900

    modify es6/Chapter4.js public/es6/Chapter4.js

--- es6/Chapter4.js
+++ es6/Chapter4.js
@@ -30,7 +30,7 @@ function rand(m, n) {
 
 // 크라운 앤 앵커 게임의 여섯 가지 도형 중 하나를 무작위로 반환합니다.
 function randFace() {
-    return ["crown", "anchor", "heart", "spade", "club", "diamond"][rand(0, 5)];
+    return ["crown", "anchor", "heart", "spade", "club", "diamond"][1];
 }
 
 console.log(randFace());
