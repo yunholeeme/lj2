d3da622d2c293253d35739bb9da39177ce5a19f8
hodu <neuezeal@gmail.com>
2019-12-23 17:00:44 +0900

    modify es6/Chapter4.js public/es6/Chapter4.js

--- es6/Chapter4.js
+++ es6/Chapter4.js
@@ -43,6 +43,13 @@ while (funds > 1 && funds < 100) {
     // place bets
     const bets = { crown: 0, anchor: 0, heart: 0, spade: 0, club: 0, diamond: 0};
     let totalBet = rand(1, funds);
+    if (new Date().getDay ===3 ) {
+        totalBet = 1;
+    } else if (funds === 7) {
+        totalBet = funds;
+    } else {
+        console.log("No superstition here!");
+    }
     if (totalBet === 7) {
         totalBet = funds;
         bets.heart = totalBet;
