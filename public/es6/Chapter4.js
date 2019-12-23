7c145ea32793b215c1e6c4e8480979fd56055a4f
hodu <neuezeal@gmail.com>
2019-12-23 17:03:14 +0900

    modify es6/Chapter4.js public/es6/Chapter4.js

--- public/es6/Chapter4.js
+++ public/es6/Chapter4.js
@@ -43,6 +43,8 @@ while (funds > 1 && funds < 100) {
     // place bets
     const bets = { crown: 0, anchor: 0, heart: 0, spade: 0, club: 0, diamond: 0};
     let totalBet = rand(1, funds);
+
+    console.log(new Date().getDay);
     if (new Date().getDay === 0 ) {
         totalBet = 1;
     } else if (funds === 7) {
