5b9e0f78b0d0ba5564ece95050c454f4555e48a9
hodu <neuezeal@gmail.com>
2019-12-24 20:51:59 +0900

    modify es6/Chapter4.js public/es6/Chapter4.js

--- es6/Chapter4.js
+++ es6/Chapter4.js
@@ -103,5 +103,8 @@ for (; s.length<10; s = ' ' + s);
 console.log(s.length);
 console.log(s);
 
-for (let x=0.2; x < 3.0; x += 0.2)
+for (let x=0.2; x < 3.0; x += 0.2)  // increment using noninteger
     console.log(x);
+
+for (; !player.isBroke;)
+    console.log("Still playing!");
