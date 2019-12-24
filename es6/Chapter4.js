58d83775c13e1b26e02772649c5cabaac43b999a
hodu <neuezeal@gmail.com>
2019-12-24 20:45:04 +0900

    modify es6/Chapter4.js public/es6/Chapter4.js

--- es6/Chapter4.js
+++ es6/Chapter4.js
@@ -96,5 +96,6 @@ console.log(`\tending funds: ${funds}`);
 for (let temp, i=0, j=1; j<30; temp = i, i = j, j = i + temp)
     console.log(j);
 
-let s = '3';    // 숫자가 들어있는 문자열
+let s = "3";    // 숫자가 들어있는 문자열
+console.log(s);
 for (; s.length<10; s = ' ' + s);
