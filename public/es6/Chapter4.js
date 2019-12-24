b26f1b2c795b74411a65e6d1c162a5d26e852ff0
hodu <neuezeal@gmail.com>
2019-12-24 21:19:27 +0900

    modify es6/Chapter4.js public/es6/Chapter4.js

--- public/es6/Chapter4.js
+++ public/es6/Chapter4.js
@@ -49,8 +49,6 @@ while (funds > 1 && funds < 100) {
         totalBet = funds;
         break;
     case 11:
-        totalBet = 0;
-        break;
     case 13:
         totalBet = 0;
         break;
